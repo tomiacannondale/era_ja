b55131f2354bfce1937b6887fecea02ec468d0f2
tomi <tomiacannondale@gmail.com>
2012-04-29 23:41:10 +0900

    Require era_ja/time in era_ja.rb

--- lib/era_ja.rb
+++ lib/era_ja.rb
@@ -1,5 +1,6 @@
 require "era_ja/version"
 require File.expand_path('era_ja/date', File.dirname(__FILE__))
+require File.expand_path('era_ja/time', File.dirname(__FILE__))
 
 module EraJa
   # Your code goes here...
