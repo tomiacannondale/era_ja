b3c29185ea1907d6cd59aced3af0764cd969ba4a
tomi <tomiacannondale@gmail.com>
2012-04-30 03:25:27 +0900

    Remove EraJa::Time module

--- lib/era_ja/time.rb
+++ lib/era_ja/time.rb
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-
-module EraJa
-  module Time
-    def to_era
-      if self < ::Time.mktime(1868,9,8)
-        raise "#to_era is expeted later in 1968,9,8"
-      elsif self < ::Time.mktime(1912,7,30)
-        "M#{year - 1867}.#{month}.#{day}"
-      elsif self < ::Time.mktime(1926,12,25)
-        "T#{year - 1911}.#{month}.#{day}"
-      elsif self < ::Time.mktime(1989,1,8)
-        "S#{year - 1925}.#{month}.#{day}"
-      else
-        "H#{year - 1988}.#{month}.#{day}"
-      end
-    end
-  end
-end
+require File.expand_path('../era_ja', File.dirname(__FILE__))
 
 class Time
-  include EraJa::Time
+  include EraJa
 end
