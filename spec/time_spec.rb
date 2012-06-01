2b830330770adeede13bcc3d5b8bb83e02952140
tomi <tomiacannondale@gmail.com>
2012-06-02 00:52:46 +0900

    Move to_era method to the shared_examples is called from the each examples in time_spec.rb

--- spec/time_spec.rb
+++ spec/time_spec.rb
@@ -6,43 +6,43 @@ describe Time do
   describe "#to_era" do
 
     context "time is 2012,4,29" do
-      subject { Time.mktime(2012,4,29).to_era }
+      subject { Time.mktime(2012,4,29) }
       include_examples "should equal 'H24.4.29'"
     end
 
     context "time is 1989,1,8" do
-      subject { Time.mktime(1989,1,8).to_era }
+      subject { Time.mktime(1989,1,8) }
 
       include_examples "should equal 'H1.1.8'"
     end
 
     context "time is 1989,1,7" do
-      subject { Time.mktime(1989,1,7).to_era }
+      subject { Time.mktime(1989,1,7) }
       include_examples "should equal 'S64.1.7'"
     end
 
     context "time is 1926,12,25" do
-      subject { Time.mktime(1926,12,25).to_era }
+      subject { Time.mktime(1926,12,25) }
       include_examples "should equal 'S1.12.25'"
     end
 
     context "time is 1926,12,24" do
-      subject { Time.mktime(1926,12,24).to_era }
+      subject { Time.mktime(1926,12,24) }
       include_examples "should equal 'T15.12.24'"
     end
 
     context "time is 1912,7,30" do
-      subject { Time.mktime(1912,7,30).to_era }
+      subject { Time.mktime(1912,7,30) }
       include_examples "should equal 'T1.7.30'"
     end
 
     context "time is 1912,7,29" do
-      subject { Time.mktime(1912,7,29).to_era }
+      subject { Time.mktime(1912,7,29) }
       include_examples "should equal 'M45.7.29'"
     end
 
     context "time is 1868,9,8" do
-      subject { Time.mktime(1868,9,8).to_era }
+      subject { Time.mktime(1868,9,8) }
       include_examples "should equal 'M1.9.8'"
     end
 
