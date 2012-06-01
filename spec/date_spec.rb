804f073498223354e2ee1cf5c2b9b04b34ec5acd
tomi <tomiacannondale@gmail.com>
2012-06-02 00:52:13 +0900

    Move to_era method to the shared_examples is called from the each examples in date_spec.rb

--- spec/date_spec.rb
+++ spec/date_spec.rb
@@ -6,43 +6,43 @@ describe Date do
   describe "#to_era" do
 
     context "date is 2012,4,29" do
-      subject { Date.new(2012,4,29).to_era }
+      subject { Date.new(2012,4,29) }
       include_examples "should equal 'H24.4.29'"
     end
 
     context "date is 1989,1,8" do
-      subject { Date.new(1989,1,8).to_era }
+      subject { Date.new(1989,1,8) }
 
       include_examples "should equal 'H1.1.8'"
     end
 
     context "date is 1989,1,7" do
-      subject { Date.new(1989,1,7).to_era }
+      subject { Date.new(1989,1,7) }
       include_examples "should equal 'S64.1.7'"
     end
 
     context "date is 1926,12,25" do
-      subject { Date.new(1926,12,25).to_era }
+      subject { Date.new(1926,12,25) }
       include_examples "should equal 'S1.12.25'"
     end
 
     context "date is 1926,12,24" do
-      subject { Date.new(1926,12,24).to_era }
+      subject { Date.new(1926,12,24) }
       include_examples "should equal 'T15.12.24'"
     end
 
     context "date is 1912,7,30" do
-      subject { Date.new(1912,7,30).to_era }
+      subject { Date.new(1912,7,30) }
       include_examples "should equal 'T1.7.30'"
     end
 
     context "date is 1912,7,29" do
-      subject { Date.new(1912,7,29).to_era }
+      subject { Date.new(1912,7,29) }
       include_examples "should equal 'M45.7.29'"
     end
 
     context "date is 1868,9,8" do
-      subject { Date.new(1868,9,8).to_era }
+      subject { Date.new(1868,9,8) }
       include_examples "should equal 'M1.9.8'"
     end
 
