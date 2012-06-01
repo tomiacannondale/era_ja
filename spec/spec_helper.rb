29512d87555ec79d0599ed50c166ec0c9aef11e9
tomi <tomiacannondale@gmail.com>
2012-06-02 00:50:40 +0900

    Move to_era method to the shared_examples is called from the each examples in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,33 +2,33 @@
 require 'rspec'
 
 shared_examples "should equal 'H24.4.29'" do
-  it { subject.should eq "H24.4.29" }
+  it { subject.to_era.should eq "H24.4.29" }
 end
 
 shared_examples "should equal 'H1.1.8'" do
-  it { subject.should eq "H1.1.8" }
+  it { subject.to_era.should eq "H1.1.8" }
 end
 
 shared_examples "should equal 'S64.1.7'" do
-  it { subject.should eq "S64.1.7" }
+  it { subject.to_era.should eq "S64.1.7" }
 end
 
 shared_examples "should equal 'S1.12.25'" do
-  it { subject.should eq "S1.12.25" }
+  it { subject.to_era.should eq "S1.12.25" }
 end
 
 shared_examples "should equal 'T15.12.24'" do
-  it { subject.should eq "T15.12.24" }
+  it { subject.to_era.should eq "T15.12.24" }
 end
 
 shared_examples "should equal 'T1.7.30'" do
-  it { subject.should eq "T1.7.30" }
+  it { subject.to_era.should eq "T1.7.30" }
 end
 
 shared_examples "should equal 'M45.7.29'" do
-  it { subject.should eq "M45.7.29" }
+  it { subject.to_era.should eq "M45.7.29" }
 end
 
 shared_examples "should equal 'M1.9.8'" do
-  it { subject.should eq "M1.9.8" }
+  it { subject.to_era.should eq "M1.9.8" }
 end
