1a59b32f98b7a7315906b21a47ea09778573739a
tomi <tomiacannondale@gmail.com>
2012-06-02 15:55:46 +0900

    Move all date format examples to spec_helper(apply time_spec)

--- spec/time_spec.rb
+++ spec/time_spec.rb
@@ -7,48 +7,43 @@ describe Time do
 
     context "time is 2012,4,29" do
       subject { Time.mktime(2012,4,29) }
-      include_examples "should equal 'H24.4.29'"
-      include_examples "should equal '平成24.4.29'"
-      include_examples "should equal '24.4.29'"
+      include_examples "2012,4,29"
     end
 
     context "time is 1989,1,8" do
       subject { Time.mktime(1989,1,8) }
 
-      include_examples "should equal 'H1.1.8'"
+      include_examples "1989,1,8"
     end
 
     context "time is 1989,1,7" do
       subject { Time.mktime(1989,1,7) }
-      include_examples "should equal 'S64.1.7'"
-      include_examples "should equal '昭和64.1.7'"
+      include_examples "1989,1,7"
     end
 
     context "time is 1926,12,25" do
       subject { Time.mktime(1926,12,25) }
-      include_examples "should equal 'S1.12.25'"
+      include_examples "1926,12,25"
     end
 
     context "time is 1926,12,24" do
       subject { Time.mktime(1926,12,24) }
-      include_examples "should equal 'T15.12.24'"
+      include_examples "1926,12,24"
     end
 
     context "time is 1912,7,30" do
       subject { Time.mktime(1912,7,30) }
-      include_examples "should equal 'T1.7.30'"
-      include_examples "should equal '大正1.7.30'"
+      include_examples "1912,7,30"
     end
 
     context "time is 1912,7,29" do
       subject { Time.mktime(1912,7,29) }
-      include_examples "should equal 'M45.7.29'"
-      include_examples "should equal '明治45.7.29'"
+      include_examples "1912,7,29"
     end
 
     context "time is 1868,9,8" do
       subject { Time.mktime(1868,9,8) }
-      include_examples "should equal 'M1.9.8'"
+      include_examples "1868,9,8"
     end
 
     context "time is 1868,9,7" do
