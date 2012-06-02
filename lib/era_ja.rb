d30a9458637786a79458646819d53c0919d1cb73
tomi <tomiacannondale@gmail.com>
2012-06-02 18:47:24 +0900

    Modify #to_era to strftime format

--- lib/era_ja.rb
+++ lib/era_ja.rb
@@ -6,17 +6,18 @@ module EraJa
   ERA = [["M", "明治"], ["T", "大正"], ["S", "昭和"], ["H", "平成"]]
 
   def to_era(format = "%o%E.%m.%d")
+    format.sub!(/%o/i) { |m| m + ' ' }
     @era_format = format[/%o/i]
     if self.to_time < ::Time.mktime(1868,9,8)
       raise "#to_era is expeted later in 1968,9,8"
     elsif self.to_time < ::Time.mktime(1912,7,30)
-      "#{format_era('M')}#{year - 1867}.#{month}.#{day}"
+      strftime(format).sub(/%E/, format_year(year - 1867)).sub(/%o /i) { format_era('M') }
     elsif self.to_time < ::Time.mktime(1926,12,25)
-      "#{format_era('T')}#{year - 1911}.#{month}.#{day}"
+      strftime(format).sub(/%E/, format_year(year - 1911)).sub(/%o /i) { format_era('T') }
     elsif self.to_time < ::Time.mktime(1989,1,8)
-      "#{format_era('S')}#{year - 1925}.#{month}.#{day}"
+      strftime(format).sub(/%E/, format_year(year - 1925)).sub(/%o /i) { format_era('S') }
     else
-      "#{format_era('H')}#{year - 1988}.#{month}.#{day}"
+      strftime(format).sub(/%E/, format_year(year - 1988)).sub(/%o /i) { format_era('H') }
     end
   end
 
@@ -27,6 +28,10 @@ module EraJa
     elsif @era_format == "%O"
       return "#{ERA.detect{ |i| i[0] == string}[1]}"
     end
+  end
+
+  def format_year(year)
+    sprintf("%02d", year)
   end
 
 end
